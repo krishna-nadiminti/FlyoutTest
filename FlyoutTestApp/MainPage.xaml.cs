5d0b98d03cf12acd66b45ac731928a1c551a91ea
Krishna <krishna.nadiminti@outlook.com>
2014-09-29 20:47:27 +1000

    Initial commit

---
+++ FlyoutTestApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace FlyoutTestApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ButtonWithPopup.Click += ButtonWithPopup_Click;
+        }
+
+        void ButtonWithPopup_Click(object sender, RoutedEventArgs e)
+        {
+            var control = new SomeFlyoutContent();
+            CreateAndShowFlyout(sender, control);
+        }
+
+        private static void CreateAndShowFlyout(
+            object anchor,
+            UIElement view)
+        {
+            var anchorElement = (FrameworkElement)anchor;
+            var flyout = FlyoutBase.GetAttachedFlyout(anchorElement) as Flyout;
+            if (flyout != null)
+            {
+                FlyoutBase.SetAttachedFlyout(anchorElement, null);
+            }
+
+            flyout = new Flyout
+            {
+                Placement = FlyoutPlacementMode.Top,
+                Content = view
+            };
+
+            flyout.Closed += (sender, o) =>
+            {
+                flyout.Content = null;
+            };
+
+            FlyoutBase.SetAttachedFlyout(anchorElement, flyout);
+
+            FlyoutBase.ShowAttachedFlyout(anchorElement);
+        }
+    }
+}
---
+++ FlyoutTestApp/SomeFlyoutContent.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace FlyoutTestApp
+{
+    public sealed partial class SomeFlyoutContent : UserControl
+    {
+        public SomeFlyoutContent()
+        {
+            InitializeComponent();
+            CreateHelperMenu();
+        }
+
+        private void CreateHelperMenu()
+        {
+            var menuFlyout = new MenuFlyout();
+
+            var values = new[] {"Why", "does", "this", "hide", "the", "parent", "flyout?"};
+
+            if (menuFlyout.Items != null)
+            {
+                foreach (var value in values)
+                {
+
+                    var menuItem = new MenuFlyoutItem
+                    {
+                        Text = value
+                    };
+
+                    menuFlyout.Items.Add(menuItem);
+                }
+            }
+            
+            FlyoutBase.SetAttachedFlyout(HelperButton, menuFlyout);
+            HelperButton.Click += (sender, args) => FlyoutBase.ShowAttachedFlyout(HelperButton);
+        }
+
+
+    }
+}
